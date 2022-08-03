8f1540c4f994b0a425cc8f4f97d4e560f2ef4c5d
emmanuelokoye <emmanuelokoye@quickcheck.ng>
2022-08-03 19:15:47 +0100

    modified contracts

--- contracts/SoulBoundFactory.sol
+++ contracts/SoulBoundFactory.sol
@@ -7,6 +7,13 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./SoulBoundNFT.sol";
 
 contract SoulBoundFactory is AccessControl, Ownable {
+    mapping(string => SoulBoundNFT) soulBound;
+    string[] soulBoundSymbols;
+
+    mapping(string => address) public deployedAddress;
+
+    SoulBoundNFT[] public soulBoundNFTs;
+
     event SoulBoundContractCreated(address soulnft);
 
     bytes32 public constant FACTORY_MANAGER = keccak256("FACTORY_MANAGER");
@@ -28,17 +35,62 @@ contract SoulBoundFactory is AccessControl, Ownable {
         transferOwnership(owner);
     }
 
-    function createSoulBoundContract(string memory name, string memory abbrev)
+    function createSoulBoundContract(string memory _name, string memory _symbol)
         public
         isPermittedFactoryManager
         returns (address soulNftContractAddress)
     {
-        SoulBoundNFT newSoulNft = new SoulBoundNFT(name, abbrev);
+        SoulBoundNFT newSoulNft = new SoulBoundNFT(_name, _symbol);
 
         soulNftContractAddress = address(newSoulNft);
 
-        emit SoulBoundContractCreated(soulNftContractAddress);
+        soulBound[_symbol] = newSoulNft;
+        soulBoundNFTs.push(newSoulNft);
 
-        return soulNftContractAddress;
+        deployedAddress[_name] = soulNftContractAddress;
+
+        emit SoulBoundContractCreated(soulNftContractAddress);
+    }
+
+    function getSoulBoundNFTSymbolsArrayLength() public view returns (uint256) {
+        return soulBoundSymbols.length;
+    }
+
+    function getSoulBoundNFTSymbolByIndex(uint256 _index)
+        public
+        view
+        returns (string memory)
+    {
+        return soulBoundSymbols[_index];
+    }
+
+    function getSoulBoundNFTAddressBySymbol(string memory _symbol)
+        public
+        view
+        returns (address)
+    {
+        return address(soulBound[_symbol]);
+    }
+
+    function getDeployedSoulBoundContracts()
+        public
+        view
+        returns (SoulBoundNFT[] memory soulNftContractAddresses)
+    {
+        soulNftContractAddresses = new SoulBoundNFT[](
+            soulNftContractAddresses.length
+        );
+        uint256 count;
+
+        for (uint256 i = 0; i < soulNftContractAddresses.length; i++) {
+            soulNftContractAddresses[count] = soulNftContractAddresses[i];
+            count++;
+        }
+    }
+
+    /// @notice Adds a new Factory Manager
+    /// @param _newFactoryManager the address of the person you are adding
+    function addFactoryManager(address _newFactoryManager) public onlyOwner {
+        grantRole(FACTORY_MANAGER, _newFactoryManager);
     }
 }
--- test/Soulbound.ts
+++ test/Soulbound.ts
@@ -3,126 +3,6 @@ import { anyValue } from '@nomicfoundation/hardhat-chai-matchers/withArgs';
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
 
-describe('Lock', function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshopt in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
-
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
-
-    // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
-
-    const Lock = await ethers.getContractFactory('Lock');
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
-  }
-
-  describe('Deployment', function () {
-    it('Should set the right unlockTime', async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.unlockTime()).to.equal(unlockTime);
-    });
-
-    it('Should set the right owner', async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.owner()).to.equal(owner.address);
-    });
-
-    it('Should receive and store the funds to lock', async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
-
-      expect(await ethers.provider.getBalance(lock.address)).to.equal(
-        lockedAmount
-      );
-    });
-
-    it('Should fail if the unlockTime is not in the future', async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await ethers.getContractFactory('Lock');
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        'Unlock time should be in the future'
-      );
-    });
-  });
-
-  describe('Withdrawals', function () {
-    describe('Validations', function () {
-      it('Should revert with the right error if called too soon', async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
-
-        await expect(lock.withdraw()).to.be.revertedWith(
-          "You can't withdraw yet"
-        );
-      });
-
-      it('Should revert with the right error if called from another account', async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
-
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner"
-        );
-      });
-
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).not.to.be.reverted;
-      });
-    });
-
-    describe('Events', function () {
-      it('Should emit an event on withdrawals', async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw())
-          .to.emit(lock, 'Withdrawal')
-          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-      });
-    });
-
-    describe('Transfers', function () {
-      it('Should transfer the funds to the owner', async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
-          [lockedAmount, -lockedAmount]
-        );
-      });
-    });
-  });
-});
-
 describe('SoulBoundFactory', function () {
   async function deploySoulBoundFactoryContract() {
     // Contracts are deployed using the first signer/account by default
@@ -130,8 +10,12 @@ describe('SoulBoundFactory', function () {
     // const owner = '0xB3910a37055df85E8EB04E12367e4DA8E24d1599';
     // let admins = ['0xEb6CD8AB0E374f3D84a4A8006dbd90b9966A4563'];
 
+    const FACTORY_MANAGER = ethers.utils.keccak256(
+      ethers.utils.toUtf8Bytes('FACTORY_MANAGER')
+    );
+
     const [owner, otherAccount] = await ethers.getSigners();
-    let admins = [otherAccount.address];
+    let admins = [otherAccount.address, owner.address];
 
     const SoulBoundFactoryContract = await ethers.getContractFactory(
       'SoulBoundFactory'
@@ -144,6 +28,56 @@ describe('SoulBoundFactory', function () {
 
     await soulBoundFactory.deployed();
 
-    return { soulBoundFactory, owner, otherAccount };
+    return {
+      soulBoundFactory,
+      owner,
+      otherAccount,
+      FACTORY_MANAGER,
+    };
   }
+
+  describe('SoulBoundFactory', function () {
+    it('soulBoundFactory should be the right owner', async function () {
+      const { soulBoundFactory, owner, otherAccount } = await loadFixture(
+        deploySoulBoundFactoryContract
+      );
+      expect(await soulBoundFactory.owner()).to.equal(owner.address);
+    });
+
+    it('soulBoundFactory should be the right admins', async function () {
+      const { soulBoundFactory, owner, otherAccount, FACTORY_MANAGER } =
+        await loadFixture(deploySoulBoundFactoryContract);
+
+      expect(
+        await soulBoundFactory.hasRole(
+          soulBoundFactory.DEFAULT_ADMIN_ROLE(),
+          owner.address
+        )
+      ).to.equal(true);
+
+      expect(
+        await soulBoundFactory.hasRole(FACTORY_MANAGER, otherAccount.address)
+      ).to.equal(true);
+    });
+
+    it('soulBoundFactory should deploy soulboundNft', async function () {
+      const { soulBoundFactory, owner, otherAccount, FACTORY_MANAGER } =
+        await loadFixture(deploySoulBoundFactoryContract);
+
+      // const txn = await soulBoundFactory.createSoulBoundContract('TEST', 'TST');
+
+      // const soulboundNft = await txn.wait();
+
+      // const event = soulboundNft.events?.find(
+      //   (event) => event.event === 'SoulBoundContractCreated'
+      // );
+
+      // const soulnft = event?.args;
+      // console.log(soulnft);
+
+      await expect(soulBoundFactory.createSoulBoundContract('TEST1', 'TST1'))
+        .to.emit(soulBoundFactory, 'SoulBoundContractCreated')
+        .withArgs(owner.address);
+    });
+  });
 });
